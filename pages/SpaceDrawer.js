4f81850bc3f922aaa0ab970844fa1ce474507111
goffygoo <jrvineetoli52@gmail.com>
2023-10-22 02:32:22 +0530

    init

--- pages/SpaceDrawer.js
+++ pages/SpaceDrawer.js
@@ -4,9 +4,13 @@ import Space from './Space';
 const Drawer = createDrawerNavigator();
 
 export default function SpaceDrawer({ route, navigation }) {
+    const spaces = ['Space 1', 'Space 2', 'Space 3', 'Space 4'];
+
     return (
         <Drawer.Navigator>
-            <Drawer.Screen name="Space 1" component={Space} />
+            {spaces.map(space =>
+                <Drawer.Screen name={space} component={Space} />
+            )}
         </Drawer.Navigator>
     )
 }
